416722c5825948f57e37e186d3ddca45d4ed109c
All <allmacdev@gmail.com>
2024-11-08 17:20:58 -0300

    create crud Cliente

--- src/controllers/cliente.js
+++ src/controllers/cliente.js
@@ -9,5 +9,36 @@ class ControllerCliente {
       res.status(500).send({ msg: error.message });
     }
   }
+  async CreateCliente(req, res) {
+    try {
+      const { nome, telefone } = req.body;
+
+      const cliente = await ServiceCliente.CreateCliente(nome, telefone);
+      res.send({ msg: cliente });
+    } catch (error) {
+      res.status(500).send({ msg: error.message });
+    }
+  }
+  async UpdatePessoa(req, res) {
+    try {
+      const id = req.params.id;
+      const { nome, telefone } = req.body;
+
+      const cliente = await ServiceCliente.UpdateCliente(id, nome, telefone);
+
+      res.send({ msg: cliente });
+    } catch (error) {
+      res.status(500).send({ msg: error.message });
+    }
+  }
+  async DeletePessoa(req, res) {
+    try {
+      const id = req.params.id;
+      await ServiceCliente.DeletePessoa(id);
+      res.status(204).send();
+    } catch (error) {
+      res.status(500).send({ msg: error.message });
+    }
+  }
 }
 module.exports = new ControllerCliente();
--- src/routers/cliente.js
+++ src/routers/cliente.js
@@ -4,5 +4,8 @@ const ControllerCliente = require("../controllers/cliente");
 const router = express.Router();
 
 router.get("/", ControllerCliente.GetClients);
+router.post("/", ControllerCliente.CreateCliente);
+router.put("/:id", ControllerCliente.UpdatePessoa);
+router.delete("/:id", ControllerCliente.DeletePessoa);
 
 module.exports = router;
